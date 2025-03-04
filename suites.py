a879894551e397d9be82d34387fb7de5081f5f19
legoff-kaelig <legoff-kaelig@users.noreply.github.com>
2025-03-04 17:20:35 +0100

    Update suites.py

--- suites.py
+++ suites.py
@@ -1,9 +1,16 @@
 #############################################################
 # Six en Suite                                              #
 # Fichier : suites.py                                       #
-# Auteur : Kaelig LE GOFF, Riwan ROUSSEAU, Jules THAMIN     #
+# Auteurs : Kaelig LE GOFF, Riwan ROUSSEAU, Jules THAMIN    #
 # Date : 03/2025                                            #
 #############################################################
 
+from sixensuite import *
+from multiples import *
 
+def is_win(liste):
+    """
+    Fonction qui renvoie True si la liste passée en paramètre est une suite gagnante, sinon False.
+    """
+    
 
