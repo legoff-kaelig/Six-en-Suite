6f232f86378acab17e91c695c7d35e17b6c432eb
Jules <thamin@350029s.ac-rennes.fr>
2025-03-04 17:31:19 +0100

    Update sixensuite.py

--- sixensuite.py
+++ sixensuite.py
@@ -7,3 +7,13 @@ for i in range(1,13): # La valeur de la Carte
         paquet.append((i, j, couleurs[((i + j) - 2) % 6]))
 shuffle(paquet) # Mélange du paquet
 
+def init():
+    nb_joueurs = int(input("Combien de joueurs participent au jeu ? (1 → 8)"))
+    main_joueurs = [ [] for _ in range(nb_joueurs)]
+    return nb_joueurs, main_joueurs
+
+# --------------------------------------------------------------------
+
+nb_joueurs, main_joueurs = init()
+
+print(nb_joueurs)
