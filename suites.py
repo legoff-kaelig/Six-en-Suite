7ebf0633ece1852218f4443af8a69868fbe1f66c
legoff-kaelig <legoff-kaelig@users.noreply.github.com>
2025-03-04 16:54:30 +0100

    Update suites.py

---
+++ suites.py
@@ -0,0 +1,7 @@
+#############################################################
+# Six en Suite                                              #
+# Fichier : suites.py                                       #
+# Auteur : Kaelig LE GOFF, Riwan ROUSSEAU, Jules THAMINS    #
+# Date : 04/03/2025                                         #
+#############################################################
+
