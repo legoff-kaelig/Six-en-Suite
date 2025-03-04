c1fb030528792769262b53fca0811286e5ab3b29
legoff-kaelig <legoff-kaelig@users.noreply.github.com>
2025-03-04 17:47:09 +0100

    Update suites.py

--- suites.py
+++ suites.py
@@ -11,6 +11,9 @@ from multiples import *
 def is_win(liste):
     """
     Fonction qui renvoie True si la liste passée en paramètre est une suite gagnante, sinon False.
+    [[carte, dé, couleur], [carte, dé, couleur], [carte, dé, couleur]]
     """
+    if len(liste) < 6:
+        return False
     
-
+    for i in range(len(liste)):
