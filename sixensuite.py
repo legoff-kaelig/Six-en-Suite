b162f795c0cf8b11e580545990c34659969d222c
Jules <thamin@350029s.ac-rennes.fr>
2025-03-10 11:55:06 +0100

    Update sixensuite.py

--- sixensuite.py
+++ sixensuite.py
@@ -12,10 +12,10 @@ from multiples import *
 def init():
     '''
     Sortie :
-        nb_joueurs →
-        main_joueurs →
+        nb_joueurs → entier positif correspondant au nombre de joueurs participant au jeu
+        main_joueurs → liste contenant ...
     
-    Permet d'initialiser la partie avec le bon nombre de joueurs
+    Permet d'initialiser la partie avec le bon nombre de joueurs, ainsi que leurs cartes.
     '''
     couleurs = ["bleu", "rouge", "vert", "jaune", "violet", "orange"]
     paquet = []
@@ -32,7 +32,21 @@ def init():
     return nb_joueurs, main_joueurs
 
 def melanger(paquet, defausse):
-    pass
+    '''
+    Entrée :
+        paquet → liste correspondant à la pioche 
+        defausse → liste correspondant à la défausse que l'on vide dans la pioche si celle-ci est vide.
+    
+    Sortie :
+        paquet → liste correspondant à la pioche
+    
+    Permet de vider la defausse dans la pioche si celle-ci est vide.
+    '''
+    if len(paquet) < 1:
+        paquet = tuple(defausse)
+        del(defausse)
+        shuffle(paquet) # Mélange du paquet
+    return paquet
 
 
 
@@ -45,5 +59,3 @@ def piocher():
 # --------------------------------------------------------------------
 
 nb_joueurs, main_joueurs = init()
-
-print(nb_joueurs)
