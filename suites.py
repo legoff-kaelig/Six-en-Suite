01b1a2b08bd100c0e0fad66313211659028e4f61
legoff-kaelig <legoff-kaelig@users.noreply.github.com>
2025-03-13 13:41:39 +0100

    Update suites.py

--- suites.py
+++ suites.py
@@ -5,8 +5,6 @@
 # Date : 03/2025                                            #
 #############################################################
 
-#import sixensuite
-# from multiples import *
 COULEURS = ["bleu", "rouge", "vert", "jaune", "violet", "orange"]
 
 def is_win(liste):
@@ -14,7 +12,7 @@ def is_win(liste):
     Fonction qui renvoie True si la liste passée en paramètre est une suite gagnante, sinon False.
     [(carte, dé, couleur), (carte, dé, couleur), (carte, dé, couleur)]
 
-    Ex de suite envoyé : [(4, 3, 'orange'), (9, 4, 'orange'), (...), (7, 3, 'vert')]
+    Ex de suite envoyé : [(4, 3, 'orange'), (9, 4, 'orange'), (7, 3, 'vert')]
 
     """
     x = 0
@@ -25,19 +23,23 @@ def is_win(liste):
     couleur = [carte[2] for carte in liste]
     valeurs = [carte[0] for carte in liste]
     
-    # Vérification des couleurs
-    nb_couleur = 0
-    for i in couleur:
-        nb_couleur = COULEURS.count(i)
-        
-        if nb_couleur >= 6:
+    # 6 même couleurs
+    for i in couleur:  
+        if couleur.count(i) >= 6:
             return True
-        
-    # Vérification des valeurs
-    nb_cartes = 0
-    for x in valeurs:
-        nb_cartes = valeurs.count(x)
-        if nb_cartes >= 6:
+
+    # 6 même valeurs
+    for x in valeurs:  
+        if valeurs.count(x) >= 6:
             return True
+
+    # Toutes les couleurs
+
+
+
+
+
+    return False
+
         
-print(is_win([(4, 3, 'orange'), (5, 4, 'vert'), (4, 3, 'vert'), (4, 2, 'vert'), (4, 1, 'vert'), (4, 1, 'vert'), (7, 1, 'vert'), (7, 1, 'vert')]))
+print(is_win([(4, 3, 'orange'), (5, 4, 'orange'), (7, 3, 'orange'), (4, 2, 'vert'), (8, 1, 'vert'), (4, 1, 'vert'), (7, 1, 'vert'), (7, 1, 'vert')]))
