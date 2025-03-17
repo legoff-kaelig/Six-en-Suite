1eec60a20444fde4a326721e405c3ec5f3481a6d
radiuum88 <radiuum@hotmail.com>
2025-03-17 20:47:35 +0100

    Ajout de commentaires (pas fini)

--- multiples.py
+++ multiples.py
@@ -17,45 +17,63 @@ def multiples(tirage):
     
     '''
 
-    verif = [(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), ("bleu", "rouge", "vert", "jaune", "violet", "orange")]
-    indiceVerif = 0
-    indiceMultiples = 0
-    currentMajority = [[0, 0]]
-    cartesMajority = [[],[]]
-    cartesPiochables = []
+    assert type(tirage) is list
+
+    verif = [(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), ("bleu", "rouge", "vert", "jaune", "violet", "orange")] # Matrice de tuples recensant les nombres et couleurs pris en compte
+    indiceTirage = 0 # Entier permettant de se localiser sur la valeur de la carte prise en compte
+    indiceMultiples = 0 # Entier permettant de notter chaque occurence d'une même valeur de manière temporaire
+    cartesMajority = [[],[]] # Matrice permettant de noter la valeur majoritaire de manière temporaire
+    currentMajority = [[0, 0]] # Matrice permettant de noter la valeur majoritaire de manière temporaire mais un peu plus longtemps quand même
+    cartesPiochables = [] # Liste des cartes piochables, la valeur retournée à la fin
+
+    #( Boucle principale qui va parcourir <verif> et prendre ces valeurs en indice <i>
 
     for i in verif :
 
+        #( Boucle secondaire qui va parcourir <i> et prendre ces valeurs en indice <j>
+
         for j in i :
+
+            #( Boucle tertiaire qui va parcourir <tirage> et prendre ces valeurs en indice <k>
 
             for k in tirage :
 
-                if j == k[indiceVerif] :
+                #( Condition qui va vérifier si la valeur de <k> en fonction de la nature de <j> est égale à l'élément <j> qui correspond à l'élément de vérification actuelle
 
-                    indiceMultiples += 1
+                if j == k[indiceTirage] :
+
+                    indiceMultiples += 1 # Ajoute 1 à l'entier <indiceMultiples> pour permettre de savoir combien d'occurences de <j> il y a dans <tirage>
                     
-                    cartesMajority[0].append(k)
+                    cartesMajority[0].append(k) # Ajoute la carte 
 
                     if k not in cartesPiochables :
 
                         cartesMajority[1].append(k)
-            
+
+                #)
+                            
+            #)
+
             if currentMajority[0][0] < indiceMultiples :
 
                 currentMajority = [[indiceMultiples, j]]
                 cartesPiochables = cartesMajority[0]
             
-            elif currentMajority[0][0] == indiceMultiples :
+            elif currentMajority[0][0] == indiceMultiples and cartesMajority[1] != [] :
 
                 currentMajority.append([indiceMultiples, j])
                 cartesPiochables.append(cartesMajority[1])
 
             cartesMajority = [[],[]]
             indiceMultiples = 0
+        
+        #)
 
-        indiceVerif = 2
+        indiceTirage = 2
+    
+    #)
 
     return cartesPiochables
 
 print(multiples([(4, 5, 'orange'), (4, 4, 'orange'), (3, 4, 'orange'), (3, 7, 'orange'), (4, 4, 'vert'), (4, 4, 'bleu')]))
-print(multiples([(4, 5, 'orange'), (4, 4, 'orange'), (3, 4, 'orange')]))
+print(multiples([(4, 5, 'orange')]))
